cd714b49479fb8de6fee273752d63bc31b7ea5cd
Varun0153 <varun@totalitycorp.com>
2024-03-27 19:12:20 +0530

    Initial Commit

---
+++ OpeninApp assignment/OpeninApp_assignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpeninApp_assignmentApp.swift
+//  OpeninApp assignment
+//
+//  Created by Varun Bagga on 27/03/24.
+//
+
+import SwiftUI
+
+@main
+struct OpeninApp_assignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
